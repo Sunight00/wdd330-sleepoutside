208c1845ca57736fb0aa89cf28eb2a948be57ad5
Sunight00 <ephraim.ewane@gmail.com>
2025-07-09 23:32:20 +0100

    last week done

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,30 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData("tents");
+import { setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+/*import { getParam } from './utils.mjs';*/
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+const dataSource = new ProductData('tents');
+/*const productId = getParam('product');
+console.log(dataSource.findProductById(productId));*/
+
+
+function addProductToCart(id,product) {
+  if (localStorage.getItem(id) !== null) {
+    setLocalStorage(id, product);
+} else {
+    setLocalStorage(id, product);
+}
+
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  addProductToCart(e.target.dataset.id,product);
 }
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
+
+// get product id from URL

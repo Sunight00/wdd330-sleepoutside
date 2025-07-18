5c7844e6c4929d9b5da32914355d4acf4eb81008
Sunight00 <ephraim.ewane@gmail.com>
2025-07-18 21:22:27 +0100

    okay

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import productData from "../js/ProductData.mjs";
 import ProductList from "../js/productList.mjs";
+import loadHeaderFooter  from "./utils.mjs";
 //import {productCardTemplate} from '../js/productList.mjs';
 
 const dataSource = new productData("tents");
@@ -9,9 +10,10 @@ const rep = new ProductList(
   dataSource,
   document.querySelector(".product-list"),
 );
-
 rep.init();
 
+
+loadHeaderFooter();
 //const pro = list.map(product => productCardTemplate(product)).join('');
 
 //document.querySelector('#h').innerHTML = pro;

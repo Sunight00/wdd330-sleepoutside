ea09b14cc45a7bd7c1a68eaa6d5ae14265701ec8
Sunight00 <ephraim.ewane@gmail.com>
2025-07-19 05:58:37 +0100

    switching good

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -6,7 +6,7 @@ export function productCardTemplate(product) {
   return `
     <li class="product-card">
       <h5>${discount}</h5>
-      <a href="product_pages/?product=${product.Id}">
+      <a href="/product_pages/?product=${product.Id}">
         <img src="${product.Image}" alt="${product.Name}">
         <h2 class="card__brand">${product.Brand.Name}</h2>
         <h3 class="card__name">${product.Name}</h3>
